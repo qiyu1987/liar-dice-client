aeb7a971390045861aedaed6425ce57baa1a10ce
qiyu1987 <qiyu1987@gmail.com>
2019-12-30 14:57:22 +0100

    global event source

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global EventSource */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react"
+import superagent from "superagent"
+
+class App extends Component {
+	url = "http://localhost:4000"
+
+	stream = new EventSource(`${this.url}/stream`)
+
+	state = {
+		text: ""
+	}
+
+	componentDidMount() {
+		this.stream.onmessage = event => {
+			const { data } = event
+
+			const action = JSON.parse(data)
+
+			console.log(action)
+		}
+	}
+
+	onSubmit = async event => {
+		event.preventDefault()
+
+		try {
+			const response = await superagent.post(`${this.url}/room`).send({
+				name: this.state.text
+			})
+
+			console.log("response test:", response)
+		} catch (error) {
+			console.warn("error test:", error)
+		}
+	}
+
+	onChange = event => {
+		// const value = event.target.value
+		const { value } = event.target
+		// const { target: { value } } = event
+
+		this.setState({ text: value })
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.onSubmit}>
+				<input type="text" onChange={this.onChange} value={this.state.text} />
+				<button>Submit</button>
+			</form>
+		)
+	}
 }
 
-export default App;
+export default App
